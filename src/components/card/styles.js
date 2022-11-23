90eabd29a5b6763c8ef8bc79556884e0a592ffd0
Omar <ofernandomm@gmail.com>
2022-11-22 22:45:11 -0500

    crear componente de card

---
+++ src/components/card/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#fff',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 8,
+        },
+        shadowOpacity: 0.44,
+        shadowRadius: 10.32,
+        elevation: 16,
+    }
+})
