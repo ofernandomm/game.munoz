2b458f9eca888b78b859604a1825b4e83fa28a4c
Omar <ofernandomm@gmail.com>
2022-11-22 23:19:09 -0500

    creando componentes y captura de numero

---
+++ src/components/input/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import colors from "../../utils/colors";
+
+export const styles = StyleSheet.create({
+    container:{
+        borderBottomWidth:colors.primary,
+        borderBottomWidth:1,
+        marginVertical:20
+    }
+})
